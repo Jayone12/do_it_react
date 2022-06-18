3dd9d09f74b4f27450f6cecf56320fa65e3c1bb0
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-06-18 18:46:55 +0900

    fixed: Destructuring assignment ES6 syntax

--- destructur_assignment/07-01.js
+++ destructur_assignment/07-01.js
@@ -1,17 +1,19 @@
 const list = [0, 1];
-let item1 = list[0];
-let item2 = list[1];
-let item3 = list[2] || -1;
-const temp = item2;
-item2 = item1;
-item1 = temp;
+let [item1, item2, item3 = -1] = list;
+
+[item2, item1] = [item1, item2];
 
 const obj = {
   key1: 'one',
   key2: 'two',
 };
 
-const key1 = obj.key1;
-const key2 = obj.key2;
-const key3 = obj.key3 || 'defalut key3 value';
-const newKey1 = obj.key1;
+const { key1: newKey1, key2, key3 = 'default key3 value' } = obj;
+
+// 구조 할당은 전개 연산자를 함께 사용한다. 
+// 구조 분해와 구조 할당은 함수 인잣값을 다루거나 JSON 데이터를 변환할 때 유용하다.
+const [item1, ...otherItems] = [0, 1, 2];
+const { key1, ...others } = { key: 'one', key: 'two' };
+
+console.log(otherItems); // [ 1, 2 ]
+console.log(others); // { key: 'two' }
